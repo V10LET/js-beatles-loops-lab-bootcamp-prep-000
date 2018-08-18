99b3f4e5eaefd255b3e1688aa64ffcbe19654635
Violet Moon <violet.wmoon@gmail.com>
2018-08-18 08:37:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let musicianName = []
+  let musicianInstrument = []
+
+  for (var musicians = 0; musicians < 4; musicians++) {
+    array[musicians]
+  }
+  return musicians
+}
